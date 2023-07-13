c54585dd9ca4e0027e3bc50998101b5c4946fa3d
Nazeekk <nazar2006erney@gmail.com>
2023-07-13 20:58:27 +0300

    edited

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import AgitationComponent from "./components/AgitationComponent/AgitationComponent";
 import PlayerComponent from "./components/PlayerComponent/PlayerComponent";
