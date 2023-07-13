fbcbf3cd77e96456631286999cc22dffb5606c5e
Nazeekk <nazar2006erney@gmail.com>
2023-07-13 20:52:19 +0300

    init

---
+++ precache-manifest.0c9ad22fcdec3889835438a91eb9bca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d788fb532b26a424750e",
+    "url": "/static/css/main.88a1014b.chunk.css"
+  },
+  {
+    "revision": "d788fb532b26a424750e",
+    "url": "/static/js/main.d788fb53.chunk.js"
+  },
+  {
+    "revision": "2c242cf1b45274adcbbc",
+    "url": "/static/js/1.2c242cf1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "997f55ed6a0e051f482af80cd864df5f",
+    "url": "/index.html"
+  }
+];
